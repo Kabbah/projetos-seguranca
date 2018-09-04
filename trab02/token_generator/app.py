072831a5ab7ba2501033201f535031910da32f95
Victor Barpp Gomes <bioniclenuparu@gmail.com>
2018-09-04 00:46:58 -0300

    Adicionado app para ser "autenticado" com token. Introdução do módulo TinyDB para armazenar/consultar/atualizar usuários.

---
+++ trab02/token_generator/app.py
@@ -0,0 +1,76 @@
+# ==============================================================================
+# app.py
+# APLICATIVO CUJO ACESSO É LIBERADO POR TOKEN (ONE TIME PASSWORD)
+#
+# Autor: Victor Barpp Gomes
+# Data: 2018-09-03
+# ==============================================================================
+"""
+Este script implementa a autenticação por token. Os usuários estão salvos no
+arquivo users.json.
+"""
+# ==============================================================================
+
+import datetime
+import getpass
+import hashlib
+import tinydb
+
+# ==============================================================================
+
+USERS_FILE = "users.json"
+
+# ==============================================================================
+
+def check_token(username, token):
+    # Lê o arquivo de usuários
+    users_data = tinydb.TinyDB(USERS_FILE)
+
+    # Busca o usuário
+    user_db = tinydb.Query()
+    user_search = users_data.search(user_db["username"] == username)
+    if len(user_search) == 0:
+        # Usuário não existe
+        return False
+    user = user_search[0]
+
+    # Verifica o último login
+    time_now = datetime.datetime.now().strftime("%Y%m%d%H%M")
+    if user["last_login"] == time_now:
+        # Senha invalidada
+        return False
+
+    # Gera tokens
+    tokens = []
+    prev_token = user["seed_pw"]
+    prev_token += user["salt"] + time_now
+    for i in range(5):
+        sha = hashlib.sha256()
+        sha.update(prev_token.encode("utf-8"))
+        prev_token = sha.hexdigest()[:6]
+        tokens.append(prev_token)
+
+    # Valida ou não o token fornecido
+    if token in tokens:
+        users_data.update({"last_login": time_now}, user_db["username"] == username)
+        return True
+    return False
+
+# ==============================================================================
+
+def main():
+    # Lê o usuário e o token
+    username = input("Username: ")
+    token = getpass.getpass("Token: ")
+
+    # Verifica o token
+    auth = check_token(username, token)
+    if auth:
+        print("Success: valid token")
+    else:
+        print("Error: invalid username/token")
+
+if __name__ == "__main__":
+    main()
+
+# ==============================================================================
---
+++ trab02/token_generator/token_generator.py
@@ -0,0 +1,120 @@
+# ==============================================================================
+# token_generator.py
+# GERADOR DE TOKENS (ONE TIME PASSWORDS)
+#
+# Autor: Victor Barpp Gomes
+# Data: 2018-09-01
+# ==============================================================================
+"""
+Este script implementa um gerador de tokens para uso com o app.py. Os usuários
+são salvos no arquivo users.json.
+"""
+# ==============================================================================
+
+import datetime
+import getpass
+import hashlib
+import random
+import tinydb
+
+# ==============================================================================
+
+ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+USERS_FILE = "users.json"
+
+# ==============================================================================
+
+def register_user():
+    # Lê o arquivo de usuários
+    users_data = tinydb.TinyDB(USERS_FILE)
+
+    # Registro de um usuário
+    username = input("Username: ")
+
+    # Verifica se já existe alguém registrado com esse nome
+    user_db = tinydb.Query()
+    user_search = users_data.search(user_db["username"] == username)
+    if len(user_search) > 0:
+        print("Error: user already exists.")
+        return
+
+    # Obtém as senhas
+    local_pw = getpass.getpass("Local password: ")
+    seed_pw = getpass.getpass("Seed password: ")
+
+    # Gera um salzinho com 4 caracteres
+    salt = "".join(random.choice(ALPHABET) for i in range(4))
+
+    # Calcula o hash da senha local
+    sha = hashlib.sha256()
+    sha.update(local_pw.encode("utf-8"))
+    sha.update(salt.encode("utf-8"))
+    hashed_local_pw = sha.hexdigest()
+
+    # Calcula o hash da senha seed
+    sha = hashlib.sha256()
+    sha.update(seed_pw.encode("utf-8"))
+    sha.update(salt.encode("utf-8"))
+    hashed_seed_pw = sha.hexdigest()
+
+    # Cria um dict com as informações do usuário
+    user_dict = {"username": username, "local_pw": hashed_local_pw,
+                 "seed_pw": hashed_seed_pw, "salt": salt,
+                 "last_login": 0}
+
+    # Adiciona o usuário à lista e grava no arquivo
+    users_data.insert(user_dict)
+
+# ==============================================================================
+
+def generate_token():
+    # Lê o arquivo de usuários
+    users_data = tinydb.TinyDB(USERS_FILE)
+
+    # Lê o usuário e a senha local
+    username = input("Username: ")
+    local_pw = getpass.getpass("Local password: ")
+
+    # Busca o usuário
+    user_db = tinydb.Query()
+    user_search = users_data.search(user_db["username"] == username)
+    if len(user_search) == 0:
+        print("Error: wrong username/password.")
+        return
+    user = user_search[0]
+
+    # Calcula o hash da senha local
+    sha = hashlib.sha256()
+    sha.update(local_pw.encode("utf-8"))
+    sha.update(user["salt"].encode("utf-8"))
+    hashed_local_pw = sha.hexdigest()
+
+    if user["local_pw"] != hashed_local_pw:
+        print("Error: wrong username/password.")
+        return
+
+    # Gera tokens
+    tokens = []
+    prev_token = user["seed_pw"]
+    prev_token += user["salt"] + datetime.datetime.now().strftime("%Y%m%d%H%M")
+    for i in range(5):
+        sha = hashlib.sha256()
+        sha.update(prev_token.encode("utf-8"))
+        prev_token = sha.hexdigest()[:6]
+        tokens.append(prev_token)
+
+    print(tokens)
+
+# ==============================================================================
+
+def main():
+    option = input("Menu:\n1 - Register new user\n2 - Generate tokens\nOption: ")
+    if option == "1":
+        register_user()
+    elif option == "2":
+        generate_token()
+
+if __name__ == "__main__":
+    main()
+
+# ==============================================================================
